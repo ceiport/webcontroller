9d2f68d5f83a371ac3bb33c9fa77f4d41de2b395
he.wang <18810243420@163.com>
2019-03-15 12:43:05 +0800

    初始项目框架搭建1.0

---
+++ src/main/java/com/ceiport/webcontroller/WebcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.ceiport.webcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebcontrollerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebcontrollerApplication.class, args);
+    }
+
+}
